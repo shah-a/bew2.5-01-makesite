f9334195a3605b668509b2a36be432fe888576f2
Ali Shah <s.ashah@outlook.com>
2021-06-30 07:41:06 -0400

    [v1.0 - step 2] Read content of `first-post.txt`

--- makesite.go
+++ makesite.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
 
 func main() {
-	fmt.Println("Salaam, world!")
+	content, err := ioutil.ReadFile("first-post.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", content)
 }
